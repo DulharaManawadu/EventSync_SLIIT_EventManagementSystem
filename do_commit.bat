5e893b24ae9dc6faffc3e4bc0b761530585f4fbd
Dewmini Iddamalgoda <it23578500@my.sliit.lk>
2026-03-29 14:15:56 +0530

    chore: remove temporary script files

--- do_commit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-git commit --no-verify -m "chore: remove client/node_modules from git tracking"
-git push --no-verify
--- final_commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add .
-git commit --no-verify -m "chore: refactored client structure and updated configurations"
-git push --no-verify
-git status
